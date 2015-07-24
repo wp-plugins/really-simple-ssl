c4aeb8607d6b79516d14b2f29d1ef95bac7430d2
RogierLankhorst <RogierLankhorst@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-24 20:38:12 +0000

    version 2.1.1
    git-svn-id: https://plugins.svn.wordpress.org/really-simple-ssl/tags/2.1.1@1205970 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rlrsssl-really-simple-ssl.php
+++ rlrsssl-really-simple-ssl.php
@@ -3,7 +3,7 @@
  * Plugin Name: Really Simple SSL
  * Plugin URI: http://www.rogierlankhorst.com/really-simple-ssl
  * Description: Lightweight plugin without any setup to make your site ssl proof
- * Version: 2.1.0
+ * Version: 2.1.1
  * Text Domain: rlrsssl-really-simple-ssl
  * Domain Path: /lang
  * Author: Rogier Lankhorst
