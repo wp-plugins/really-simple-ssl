d195673ff0aaefae6846f8f6e56e0e8470462c69
RogierLankhorst <RogierLankhorst@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-20 12:59:14 +0000

    2.2.3, after 2.2.2
    git-svn-id: https://plugins.svn.wordpress.org/really-simple-ssl/tags/2.2.2@1290690 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ssl-test-page.php
+++ ssl-test-page.php
@@ -1,51 +1,46 @@
 <html>
-<head></head>
+<head>
+ <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+</head>
 <body>
 <h1>SSL test page</h1>
 <p>This page is used purely to test for ssl availability.</p>
 <?php
-	function is_ssl() {
-		if ( isset($_SERVER['HTTPS']) ) {
-			if ( 'on' == strtolower($_SERVER['HTTPS']) )
-				return true;
-			if ( '1' == $_SERVER['HTTPS'] )
-				return true;
-		} elseif ( isset($_SERVER['SERVER_PORT']) && ( '443' == $_SERVER['SERVER_PORT'] ) ) {
-			return true;
+	$ssl = FALSE;
+	if (isset($_SERVER['HTTPS']) ) {
+		if ( strtolower($_SERVER['HTTPS']) == 'on') {
+			echo "#SERVER-HTTPS-ON#"." (".$_SERVER['HTTPS'].")<br>";
+			$ssl = TRUE;
 		}
-		return false;
+		if ( '1' == $_SERVER['HTTPS'] ) {
+			echo "#SERVER-HTTPS-1#<br>";
+			$ssl = TRUE;
+		}
+	}
+	if (isset($_SERVER['SERVER_PORT']) && ( '443' == $_SERVER['SERVER_PORT'] )) {
+			echo "#SERVERPORT443#<br>";
+			$ssl = TRUE;
+	}
+	if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')){
+		echo "#LOADBALANCER#<br>";
+		$ssl = TRUE;
+	}
+	if (!empty($_SERVER['HTTP_X_FORWARDED_SSL']) && ($_SERVER['HTTP_X_FORWARDED_SSL'] == 'on')){
+		echo "#CDN#<br>";
+		$ssl = TRUE;
 	}
 
-	function ssl_behind_load_balancer() {
-		if(!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
-			return TRUE;
-		}
-		else {
-			return FALSE;
-		}
+	if ($ssl) {
+		echo "<br>#SUCCESFULLY DETECTED SSL#";
+	} else {
+		echo "<br>#NO KNOWN SSL CONFIGURATION DETECTED#";
 	}
-
-	function ssl_behind_cdn() {
-		if(!empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
-			return TRUE;
-		}
-		else {
-			return FALSE;
-		}
-	}
-
-	if (is_ssl()) {
-	 	echo "#STANDARD-SSL#";
-	}
-	elseif (ssl_behind_load_balancer()){
-		echo "#LOADBALANCER#";
-	}
-	elseif (ssl_behind_cdn()){
-		echo "#CDN#";
-	}
-	else {
-		echo "#NO SSL DETECTED#";
-	}
+?>
+<br><br><br>
+<?php
+  echo "HTTP_HOST: ".$_SERVER["HTTP_HOST"];
+  echo "<br>";
+  echo "REQUEST_URI: ".$_SERVER["REQUEST_URI"];
 ?>
 </body>
 </html>
